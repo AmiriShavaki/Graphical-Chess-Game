b7b1fcea10f6c85896300586a1c2b27cdfd10169
Mahdi Amiri Shavaki <mahdi_amiri98@comp.iust.ac.ir>
2020-01-08 19:21:31 +0330

    cell class is now prepared

--- main.cpp
+++ main.cpp
@@ -3,7 +3,52 @@
 
 using namespace std;
 
+class cell{
+public:
+    cell(int, int);
+    int getX();
+    int getY();
+private:
+    int x;
+    int y;
+};
+
+cell::cell(int givenX, int givenY) {
+    x = givenX;
+    y = givenY;
+}
+
+int cell::getX() {
+    return x;
+}
+
+int cell::getY() {
+    return y;
+}
+
+/*
+class piece {
+public:
+    piece();
+private:
+    cell pos;
+};
+
+class board {
+public:
+    board();
+private:
+    bool gameIsOver;
+};
+
+board::board() {
+    gameIsOver = 0;
+}
+*/
 int main() {
     initwindow(800,600);
-	getch();
+    /* In case we want to test cell class
+    cell tst(100, 200);
+    cout << tst.getX() << tst.getY(); */
+    getch();
 }
